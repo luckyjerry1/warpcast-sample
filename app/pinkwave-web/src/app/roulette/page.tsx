9710bdc9a80e32cc5a8f15c6fc4b8b8a23409c84
luckyjerry <luckyjerry422@gmail.com>
2025-01-23 23:28:49 +0900

    change image, change button text

--- app/pinkwave-web/src/app/roulette/page.tsx
+++ app/pinkwave-web/src/app/roulette/page.tsx
@@ -4,13 +4,13 @@ import { NEXT_PUBLIC_URL } from '../config';
 export const metadata: Metadata = {
   other: {
     'fc:frame': 'vNext',
-    'fc:frame:button:1': 'Shoot',
+    'fc:frame:button:1': '🎯 Try Your Luck!',
     'fc:frame:button:1:action': 'post',
     'fc:frame:button:1:post_url': `${NEXT_PUBLIC_URL}/api/roulette/shoot`,
-    'fc:frame:button:2': 'Stop',
+    'fc:frame:button:2': '🎲 Show Result',
     'fc:frame:button:2:action': 'post',
     'fc:frame:button:2:post_url': `${NEXT_PUBLIC_URL}/api/roulette/stop`,
-    'fc:frame:image': `${NEXT_PUBLIC_URL}/roulette/hey.png`,
+    'fc:frame:image': `${NEXT_PUBLIC_URL}/roulette/new image.png`,
     'fc:frame:image:aspect_ratio': '1:1',
   },
   title: 'roulette',
