e65a823c9120dd4390e09b549e850b881319be72
luckyjerry <luckyjerry422@gmail.com>
2025-01-24 01:52:19 +0900

    set page 3

---
+++ app/pinkwave-web/src/app/api/roulette/shoot2/route.tsx
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { NEXT_PUBLIC_URL } from '../../../config';
+import { getFrameHtmlResponse } from '../../../../utils/getFrameHtmlResponse';
+
+async function getResponse(): Promise<NextResponse> {
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Mint Mini Building',
+          action: 'post',
+          target: `${NEXT_PUBLIC_URL}/api/roulette/shoot`
+        },
+        {
+          label: 'Go back',
+          action: 'post',
+          target: `${NEXT_PUBLIC_URL}/api/roulette/roulette`
+        }
+      ],
+      image: {
+        aspectRatio: '1:1',
+        src: `${NEXT_PUBLIC_URL}/roulette/page3.png`,
+      },
+    }),
+  );
+}
+
+export async function POST(): Promise<Response> {
+  return getResponse();
+}
+
+export const dynamic = 'force-dynamic';
